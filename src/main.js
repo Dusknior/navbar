9e4c58ece96f6917996c7f2329b27f8ab52756ef
Dusknior <danielbernalherrera@gmail.com>
2021-10-25 22:49:20 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-vue.css'
 
 createApp(App).mount('#app')
